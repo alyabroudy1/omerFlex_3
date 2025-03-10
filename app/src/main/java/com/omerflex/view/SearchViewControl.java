c0dcd715761f1799f9cc14d5b1f1618cab56e84c
alyabroudy1 <alyabroudy1@gmail.com>
2025-03-10 12:36:34 +0100

    new build

--- app/src/main/java/com/omerflex/view/SearchViewControl.java
+++ app/src/main/java/com/omerflex/view/SearchViewControl.java
@@ -417,16 +417,16 @@ public abstract class SearchViewControl {
 //                }
                 Log.d(TAG, "loadCategoriesInBackground: " + server.getServerId());
                 if (
-                        server instanceof OldAkwamServer ||
-                                server instanceof AkwamServer ||
-                                server instanceof ArabSeedServer ||
+//                        server instanceof OldAkwamServer ||
+//                                server instanceof AkwamServer ||
+//                                server instanceof ArabSeedServer ||
 //                                server instanceof CimaNowServer ||
-                                server instanceof FaselHdServer ||
-                                server instanceof OmarServer ||
-                                server instanceof IptvServer ||
-                                server instanceof MyCimaServer ||
-                                server instanceof KooraServer
-//                        (server instanceof KooraServer && !query.isEmpty())
+//                                server instanceof FaselHdServer ||
+//                                server instanceof OmarServer ||
+//                                server instanceof IptvServer ||
+//                                server instanceof MyCimaServer ||
+//                                server instanceof KooraServer
+                        (server instanceof KooraServer && !query.isEmpty())
                 ) {
                     continue;
                 }
